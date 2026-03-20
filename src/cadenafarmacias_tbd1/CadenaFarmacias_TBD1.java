32d604a8a7cdeddf49b669d374a7b996016e67af
David Castro <dscastrogarcia@gmail.com>
2026-03-20 12:15:35 -0600

    solo se creo un tabbed pane

--- src/cadenafarmacias_tbd1/CadenaFarmacias_TBD1.java
+++ src/cadenafarmacias_tbd1/CadenaFarmacias_TBD1.java
@@ -23,17 +23,45 @@ public class CadenaFarmacias_TBD1 extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        pantallaPanel = new javax.swing.JPanel();
+        pantallaTabbedPane = new javax.swing.JTabbedPane();
+        jPanel1 = new javax.swing.JPanel();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 807, Short.MAX_VALUE)
+        );
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 521, Short.MAX_VALUE)
+        );
+
+        pantallaTabbedPane.addTab("tab1", jPanel1);
+
+        javax.swing.GroupLayout pantallaPanelLayout = new javax.swing.GroupLayout(pantallaPanel);
+        pantallaPanel.setLayout(pantallaPanelLayout);
+        pantallaPanelLayout.setHorizontalGroup(
+            pantallaPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(pantallaTabbedPane)
+        );
+        pantallaPanelLayout.setVerticalGroup(
+            pantallaPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(pantallaTabbedPane, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, 556, Short.MAX_VALUE)
+        );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addComponent(pantallaPanel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addComponent(pantallaPanel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
 
         pack();
@@ -65,5 +93,8 @@ public class CadenaFarmacias_TBD1 extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JPanel pantallaPanel;
+    private javax.swing.JTabbedPane pantallaTabbedPane;
     // End of variables declaration//GEN-END:variables
 }
